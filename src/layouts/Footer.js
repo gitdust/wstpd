f0798635e4acbfbcde364cafa4cec4011068c658
gitdust <fchao313@gmail.com>
2018-05-17 19:47:18 +0800

    style: 添加备案号

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import BaseLink from '@/components/BaseLink';
+
+const styles = {
+  copy: {
+    fontSize: '16px',
+    display: 'block',
+  },
+  beian: {
+    fontSize: '12px',
+    display: 'block',
+  },
+}
 
 const Footer = () => {
   return (
     <footer>
-      &copy; 2018 what&#39;s the package do?
+      <span style={styles.copy}>&copy; 2018 what&#39;s the package do?</span>
+      <span style={styles.beian}><BaseLink url="http://www.miitbeian.gov.cn/">皖ICP备17026479号-2</BaseLink></span>
     </footer>
   );
 }
