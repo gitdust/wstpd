a9dac4485ecf9d9bf114add982358f72fd105e4f
gitdust <fchao313@gmail.com>
2018-05-25 19:51:24 +0800

    chore: uglifyjs已支持多线程

--- webpack/utils.js
+++ webpack/utils.js
@@ -6,7 +6,7 @@ const PRO = process.env.NODE_ENV === 'production';
 const os = require('os');
 const path = require('path');
 const webpack = require('webpack');
-const UglifyJsParallelPlugin = require('webpack-uglify-parallel');
+const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 const HappyPack = require('happypack');
 
 // cpu 个数
@@ -17,28 +17,12 @@ const DefinePlugin = () => new webpack.DefinePlugin({
   'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`,
 });
 
-// JS 压缩、混淆插件
-const UglifyJsPlugin = () => new UglifyJsParallelPlugin({
-  workers: CPUS,
+// https://github.com/mishoo/UglifyJS2/tree/harmony#minify-options
+const UglifyJsPlugin = () => new UglifyJsWebpackPlugin({
+  test: /\.js$/i,
+  cache: true,
+  parallel: CPUS,
   sourceMap: true,
-  // 最紧凑的输出
-  beautify: false,
-  // 删除所有的注释
-  comments: false,
-  mangle: true,
-  compressor: {
-    // 在UglifyJs删除没有用到的代码时不输出警告
-    warnings: false,
-    // 不删除所有的 `console` 语句
-    drop_console: false,
-    // 内嵌定义了但是只用到一次的变量
-    collapse_vars: true,
-    drop_debugger: true,
-    // 提取出出现多次但是没有定义成变量去引用的静态值
-    reduce_vars: true,
-    // 用于 tree-shaking,属性 getters 不存在 side effects
-    pure_getters: true,
-  },
 });
 
 // 多线程编译插件
