71e7f30ac68184129aa448c6b2dc356beb0e97b3
gitdust <fchao313@gmail.com>
2018-07-05 19:53:58 +0800

    chore: 新增错误处理

--- src/utils/error.js
+++ src/utils/error.js
@@ -3,7 +3,7 @@ import * as globalMessage from './feedback';
 
 window.onerror = (message, source, lineno, colno) => {
   // 收集客户端错误信息
-  const msg = {}; 
+  const msg = {};
   msg.userAgent = window.navigator.userAgent;
   msg.message = message;
   msg.source = source;
@@ -20,3 +20,13 @@ window.onerror = (message, source, lineno, colno) => {
   // TODO: 运行时错误跳转页面
   globalMessage.error(message);
 };
+
+// 资源加载出错，比如 image 和  script
+window.addEventListener('error', event => {
+  console.log('resources load error: ', event);
+});
+
+// 未 catch 的 promise 异常
+window.addEventListener('unhandledrejection', (event) => {
+  console.warn(`WARNING: Unhandled promise rejection. Shame on you! Reason: ${event.reason}`);
+});
